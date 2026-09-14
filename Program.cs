17514c5497721b5fd81df8c292fa9a9bebe40aae
Victor Rodrigues <1261946761@ulife.com.br>
2026-09-13 21:27:53 -0300

    Exercício de C# (ViaCep)

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsumerViaCep;
+using static System.Console;
+
+WriteLine("Digite o CEP que deseja consultar:");
+
+var cep = ReadLine();
+
+var enderecoUrl = $"https://viacep.com.br/ws/{cep}/json/";
+
+WriteLine($"Consultando o endereço na url: {enderecoUrl}");
+
+var cliente = new HttpClient();
+
+try
+{
+    
+    HttpResponseMessage response = await cliente.GetAsync(enderecoUrl);
+    response.EnsureSuccessStatusCode();
+
+    String respostaApi = await response.Content.ReadAsStringAsync();
+
+    Endereco? endereco = System.Text.Json.JsonSerializer.Deserialize<Endereco>(respostaApi);
+
+    if (endereco !=null)
+    {
+
+    WriteLine($"\nCEP: {endereco.Cep}");
+    WriteLine($"\nRua: {endereco.Logradouro}");
+    WriteLine($"\nComplemento: {endereco.Complemento}");
+    WriteLine($"\nBairro: {endereco.Bairro}");
+    WriteLine($"\nLocalidade: {endereco.Localidade}");
+    WriteLine($"\nUF: {endereco.UF}");
+
+    }
+}
+
+catch (Exception ex)
+{
+    
+    WriteLine ($"Ocorreu um erro ao consultar o endereço: {ex.Message}");
+
+}
